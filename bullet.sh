a689e7ee72752df6622e5da4d2e457c99d02a23c
devbence <936368+devbence@users.noreply.github.com>
2024-05-14 00:10:38 +0200

    add BACKENDIP var manual option

--- bullet.sh
+++ bullet.sh
@@ -4,6 +4,7 @@
 SIT_VERSION="${SIT:=latest}"
 HEADLESS="${HEADLESS:-}"
 FORCE="${FORCE:-}"
+BACKENDIP="${BACKENDIP:=$(curl -s4 ipv4.icanhazip.com)}"
 echo "Stay In Tarkov Docker"
 echo "github.com/StayInTarkov"
 
@@ -18,13 +19,12 @@ if [ ! -e "/opt/server/version" ] || [ ! -z "$FORCE" ]; then
     echo "Starting the server to generate all the required files.. Please wait."
     cd /opt/server
     chown $(id -u):$(id -g) ./* -Rf
-    EXT_IP=$(curl -4 ipv4.icanhazip.com)
     sed -i 's/\"ip\": \"127.0.0.1\"/\"ip\": \"0.0.0.0\"/g' ./Aki_Data/Server/configs/http.json
-    sed -i "s/\"backendIp\": \"127.0.0.1\"/\"backendIp\": \"$EXT_IP\"/g" ./Aki_Data/Server/configs/http.json
+    sed -i "s/\"backendIp\": \"127.0.0.1\"/\"backendIp\": \"$BACKENDIP\"/g" ./Aki_Data/Server/configs/http.json
     NODE_CHANNEL_FD= timeout 40s ./Aki.Server.exe </dev/null >/dev/null 2>&1
     echo "Version $SIT_VERSION is set to ./server/version"
     echo "$SIT_VERSION" > /opt/server/version
-    if [[ -z $HEADLESS ]]; then
+    if [ -z "$HEADLESS" ]; then
       echo "SIT.Docker setup is now complete!"
       echo "You can configure and start your container."
       exit 0
